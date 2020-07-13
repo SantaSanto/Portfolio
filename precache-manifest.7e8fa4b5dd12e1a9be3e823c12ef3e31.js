097c82a2b0bf72c5795a7ac45d88b3249ac5f159
SantaSanto <SanthoshKumar.Mail@Gmail.com>
2020-07-13 19:46:32 +0530

    Added MUI Build

---
+++ precache-manifest.7e8fa4b5dd12e1a9be3e823c12ef3e31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2455d1f8f6684ed208f5738f7db6113b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e346b5f1115ca7719991",
+    "url": "/static/js/2.5a91c1c2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.5a91c1c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00ccaa131f38345efb07",
+    "url": "/static/js/main.c955f063.chunk.js"
+  },
+  {
+    "revision": "842d2729e3814803514c",
+    "url": "/static/js/runtime-main.306bd4b9.js"
+  }
+]);
